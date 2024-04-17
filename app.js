b6ac4c26c6c18c004f8588b7c1da4d3439dd72aa
Jarib Cockerel <jarib.cockerel@outlook.com>
2024-04-17 09:58:54 -0500

    deconstrcuted values to plug into variable when called. If that value was empty, then the default value would show as undefined

--- app.js
+++ app.js
@@ -49,3 +49,26 @@ let planetFacts = {
 let { numPlanets, ...discoveryYears } = planetFacts;
 
 console.log(discoveryYears);
+
+/*
+Object Destructing 3
+
+what does the following code return/print
+
+function getUserData({firstName, favoriteColor = 'green'}){
+    return `your name is ${firstName} and you like ${favoriteColor}`;
+}
+
+getUserData({firstName: "Alejandro", favoriteColor: "purple"})// 'Your name is Alejandro and you like purple.
+
+
+getUserData({firstName: Melissa})//Your name is Melissa and you like green.
+
+
+getUserData({})//Your name is undefined and you like green 
+
+*/
+
+function getUserData({ firstName, favoriteColor = "green" }) {
+  return `your name is ${firstName} and you like ${favoriteColor}`;
+}
